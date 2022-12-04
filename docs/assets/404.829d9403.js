b74ebb23dca39a9f120c3dd49eab9b98e16b11da
Cheng-DX <2337040896@qq.com>
2022-12-04 18:00:51 +0800

    fix

---
+++ docs/assets/404.829d9403.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue:export-helper.1ba7d2c9.js";import{a as t,o as r,h as n,w as s,g as c}from"./index.74a651f6.js";const a={};function _(l,f){const o=t("router-link");return r(),n(o,{to:"/blog-list"},{default:s(()=>[c(" Blogs ")]),_:1})}const p=e(a,[["render",_]]);export{p as default};
