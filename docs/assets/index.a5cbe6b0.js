b7cf81c2de425872490088bf552f96497386b44f
Cheng-DX <2337040896@qq.com>
2022-12-06 19:45:04 +0800

    chore: update

--- docs/assets/index.a5cbe6b0.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue:export-helper.1ba7d2c9.js";import{a as r,o as t,h as n}from"./index.74a651f6.js";const c={};function s(_,a){const e=r("router-view");return t(),n(e)}const m=o(c,[["render",s]]);export{m as default};
---
+++ docs/assets/index.b43db420.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue:export-helper.1ba7d2c9.js";import{a as r,o as t,i as n}from"./index.19e7706e.js";const c={};function s(_,a){const e=r("router-view");return t(),n(e)}const m=o(c,[["render",s]]);export{m as default};
